d626f0279048acca47f8486b9a6c7b2b8e636ded
Msaezcardenas <msaezcardernas@gmail.com>
2019-03-01 14:05:50 -0300

    lectura y extracción de links

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = () => {
-  // ...
-};
+const md = require('./prueba.js');
+
+if (require.main === module) {
+  console.log ('Ingresa archivo o carpeta');
+  md.mdPathLinks();
+}
+
+
+// const readline = require('readline');
+// const fs = require('fs');
+
+// let myInterface = readline.createInterface({
+//   input: fs.createReadStream('README.md')
+// });
+
+// const printData= (data) => {
+//   console.log(`${data}`);
+// };
+
+// myInterface.on('line', printData);
---
+++ prueba.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+//llama al modulo que extrae los links del texto
+const markdownLinkExtractor = require('markdown-link-extractor');
+
+// console.log(extractlinkMd);
+
+
+
+//Guarda en esta variable la ruta del archivo. Verifica si es ruta absoluta o relativa 
+const mdPathLinks = function urlFilter(path,option){
+  console.log(path);
+
+  //si es carpeta...
+  const pathFolder = String(path).trim();  //elimina espacios en la ruta ingresada por el usuario
+  // con la sig, linea me devuelve  un -1 si la ruta existe o no 
+
+  if (String(pathFolder).indexOf('md') !== -1){
+
+    //guarda la lectura del archivo
+    const extractlinkMd = fs.readFileSync(path).toString();
+    
+    //extrae los links del archivo MD
+    const links = markdownLinkExtractor(extractlinkMd);
+    links.forEach(function (line){
+        console.log(line)
+    });
+      
+  }
+};
+
+
+
+module.exports = mdPathLinks;
