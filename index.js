e4438eec7e57d3ee9f519b9c3ccf41b1cf659448
Msaezcardenas <msaezcardernas@gmail.com>
2019-03-07 08:23:00 -0300

    se lee archivo

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const readLine = require('readline');
-const md = require('./prueba.js');
+// const readLine = require('readline');
+// const md = require('./prueba.js');
 
-// Pausa en consola, para que el usuario escriba
-if (require.main === module) {
-  console.log ('Ingresa archivo o carpeta');
-  const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false,
-  });
+// // Pausa en consola, para que el usuario escriba
+// if (require.main === module) {
+//   console.log ('Ingresa archivo o carpeta');
+//   const rl = readLine.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//     terminal: false,
+//   });
 
- // captura lo que escribió el usuario
-  rl.on('line', (line) => {
-    let arrayCapture = [];
-    arrayCapture = line.split('--');
-    const path = arrayCapture[0];
-    const option = String(arrayCapture[1]).trim();
-    md.mdPathLinks(path);
-    rl.close();
-  });
-}
+//  // captura lo que escribió el usuario
+//   rl.on('line', (line) => {
+//     let arrayCapture = [];
+//     arrayCapture = line.split('--');
+//     const path = arrayCapture[0];
+//     const option = String(arrayCapture[1]).trim();
+//     md.mdPathLinks(path);
+//     rl.close();
+//   });
+// }
 
 
 
--- prueba.js
+++ prueba.js
@@ -1,31 +1,54 @@
 const fs = require('fs');
+const readline = require('readline');
+const markdownLinkExtractor = require('markdown-link-extractor'); // extrae links
 
-//llama al modulo que extrae los links del texto
-const markdownLinkExtractor = require('markdown-link-extractor');
-
-module.exports ={ 
-
-//Guarda en esta variable la ruta del archivo. Verifica si es ruta absoluta o relativa 
-mdPathLinks: (path,option) => {
-
-  //si es carpeta...
-  const pathFolder = String(path).trim();  //elimina espacios en la ruta ingresada por el usuario
-  
-  // con la sig, linea me devuelve  un -1 si la ruta existe o no 
-  if (String(pathFolder).indexOf('md')!== -1){
-
-    //guarda la lectura del archivo
-    const extractlinkMd = fs.readFileSync(path).toString();
+// fs.stat(path,(error, stats)) => {
+//   if (stats.isFile()){
     
-    //extrae los links del archivo MD
-    const links = markdownLinkExtractor(extractlinkMd);
+//   } 
+// }
 
-    links.forEach(function (line){
-        console.log(line)
-    });
-  }
+  
 
-},
-};
+//se lee el archivo
+fs.readFile("README.md", (error, data)=>{
+  console.log(data.toString());
+})
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// //llama al modulo que extrae los links del texto
+// const markdownLinkExtractor = require('markdown-link-extractor');
+
+// module.exports ={ 
+
+// //Guarda en esta variable la ruta del archivo. Verifica si es ruta absoluta o relativa 
+// mdPathLinks: (path,option) => {
+
+//   //si es carpeta...
+//   const pathFolder = String(path).trim();  //elimina espacios en la ruta ingresada por el usuario
+  
+//   // con la sig, linea me devuelve  un -1 si la ruta existe o no 
+//   if (String(pathFolder).indexOf('md')!== -1){
+
+//     //guarda la lectura del archivo
+//     const extractlinkMd = fs.readFileSync(path).toString();
+    
+//     //extrae los links del archivo MD
+//     const links = markdownLinkExtractor(extractlinkMd);
+
+//     links.forEach(function (line){
+//         console.log(line)
+//     });
+//   }
+
+// },
+// };
 
 
