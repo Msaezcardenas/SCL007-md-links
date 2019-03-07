84ce5d231154a6832391cada239a9979730d1cdb
Msaezcardenas <msaezcardernas@gmail.com>
2019-03-07 12:51:42 -0300

    se extraen links

--- prueba.js
+++ prueba.js
@@ -2,15 +2,22 @@ const fs = require('fs');
 const readline = require('readline');
 const markdownLinkExtractor = require('markdown-link-extractor'); // extrae links
 
+//defino mi función...
 const mdlinks = (path, options) => {
   //entrega la ruta
   fs.stat(path, (error, stats) => {
     //stats describe si es un directorio o un archivo
     if (stats.isFile()) {
       //se lee el archivo
-      fs.readFile("README.md", (error, data) => {
-        console.log(data.toString());
+      // fs.readFile(path, (error, data) => {
+      //   console.log(data.toString());
+      // });
+      let markdown = fs.readFileSync(path).toString();
+      let links = markdownLinkExtractor(markdown);
+      links.forEach(function (link) {
+        console.log(link);
       });
+
     } else if (stats.isDirectory()) {
       //se lee el directorio 
       fs.readdir(path, (error, files) => {});
@@ -18,4 +25,13 @@ const mdlinks = (path, options) => {
   });
 };
 
-mdlinks('README.md');
+// var markdown = fs.readFileSync('READMEproyecto.md').toString();
+
+// var links = markdownLinkExtractor(markdown);
+
+// links.forEach(function (link) {
+//   console.log(link);
+// });
+
+// // La uso...
+mdlinks('READMEproyecto.md');
